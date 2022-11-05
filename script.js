878a85a9d352ed753163381746e7a40b33802dac
Sergio <sergioajk@hotmail.com>
2022-11-05 12:29:01 -0300

    botão apagar

--- script.js
+++ script.js
@@ -8,33 +8,46 @@
 // captura dos botões
 const buttonInputBoard = document.getElementById('board-size');
 const buttonVQV = document.getElementById('generate-board');
-const buttonLimpar = document.getElementById('clear-board');
 // botão cor
 
 const getButtonCorAleatoria = document.getElementById('button-random-color');
+const getTrocaCorClass2 = document.querySelector('.secondColor');
+const getTrocaCorClass3 = document.querySelector('.thirdColor');
+const getTrocaCorClass4 = document.querySelector('.fourtColor');
 getButtonCorAleatoria.addEventListener('click', () => {
-    const getTrocaCorClass2 = document.querySelector('.secondColor');
-    const getTrocaCorClass3 = document.querySelector('.thirdColor');
-    const getTrocaCorClass4 = document.querySelector('.fourtColor');
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     let corRGB = `rgb(${r},${g},${b})`;
-    console.log(corRGB);
+    // console.log(corRGB);
     getTrocaCorClass2.style.background = corRGB;
     r = Math.floor(Math.random() * 255);
     g = Math.floor(Math.random() * 255);
     b = Math.floor(Math.random() * 255);
     corRGB = `rgb(${r},${g},${b})`;
     getTrocaCorClass3.style.background = corRGB;
-    console.log(corRGB);
+    // console.log(corRGB);
     r = Math.floor(Math.random() * 255);
     g = Math.floor(Math.random() * 255);
     b = Math.floor(Math.random() * 255);
     corRGB = `rgb(${r},${g},${b})`;
     getTrocaCorClass4.style.background = corRGB;
-    console.log(corRGB);
+    // console.log(corRGB);
 });
 // Botão apagar
+const getButtonLimpar = document.getElementById('clear-board');
+const getPixelClass = document.querySelectorAll('.pixel');
+console.log(getPixelClass)
+const corApagar = 'rgb(255,255,255)'
+getButtonLimpar.addEventListener('click', () => {
+    for (index = 0; index < getPixelClass.length; index += 1) {
+        if (getPixelClass[index].style.background !== 'rgb(255,255,255)'){
+        getPixelClass[index].style.background = corApagar;
+    }
+}
+});
+
+
+
 // console.log(getButtonCorAleatoria);
 // console.log(getTrocaCorClass);
