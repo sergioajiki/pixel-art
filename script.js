643bd9f3b316d3c2a66130d61c972771ac5e60d7
Sergio <sergioajk@hotmail.com>
2022-11-12 21:22:51 -0300

    requisito 15

--- script.js
+++ script.js
@@ -15,7 +15,14 @@ window.onload = function () {
     const corInicial4 = document.querySelector('.fourtColor');
     corInicial4.style.backgroundColor = recoveryColor.guardaCor4;
   }
-}
+  if (localStorage.getItem('boardSize') === null) {
+    let N = 5;
+    localStorage.setItem('boardSize', JSON.stringify(N));
+    boardInicio();
+    geral();
+    return
+  }
+};
 // captura dos botões
 // botão input e VQV
 const getButtonInputBoard = document.getElementById('board-size');
@@ -41,15 +48,26 @@ getButtonVQV.addEventListener('click', () => {
     for (let index = 0; index < linha; index += 1) {
       const pixelDoQuadro = document.createElement('div');
       pixelDoQuadro.className = 'pixel';
-      pixelDoQuadro.style.backgroundColor = 'rgb(255,255,255)';
-      pixelDoQuadro.style.height = '40px';
-      pixelDoQuadro.style.width = '40px';
-      pixelDoQuadro.style.border = '1px solid black';
-      pixelDoQuadro.style.display = 'inline-block';
+      // pixelDoQuadro.style.backgroundColor = 'rgb(255,255,255)';
+      // pixelDoQuadro.style.height = '40px';
+      // pixelDoQuadro.style.width = '40px';
+      // pixelDoQuadro.style.border = '1px solid black';
+      // pixelDoQuadro.style.display = 'inline-block';
+
+      pixelDoQuadro.addEventListener('mousedown', (event) => {
+        // event.target;
+        const pincel = document.querySelector('.selected');
+        const corDoPincel = pincel.style.backgroundColor;
+        // console.log(getPixel);
+        console.log(corDoPincel);
+        pixelDoQuadro.style.backgroundColor = corDoPincel;
+      });
       mainBoard.appendChild(pixelDoQuadro);
     }
     mainBoard.appendChild(document.createElement('br'));
+
   }
+  localStorage.setItem('boardSize', JSON.stringify(N));
 });
 
 // botão cor aleatoria
@@ -62,29 +80,29 @@ getButtonCorAleatoria.addEventListener('click', () => {
   let g = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
   let corRGB = `rgb(${r},${g},${b})`;
-  // console.log(corRGB);
+
   getTrocaCorClass2.style.backgroundColor = corRGB;
   r = Math.floor(Math.random() * 255);
   g = Math.floor(Math.random() * 255);
   b = Math.floor(Math.random() * 255);
   corRGB = `rgb(${r},${g},${b})`;
   getTrocaCorClass3.style.backgroundColor = corRGB;
-  // console.log(corRGB);
+
   r = Math.floor(Math.random() * 255);
   g = Math.floor(Math.random() * 255);
   b = Math.floor(Math.random() * 255);
   corRGB = `rgb(${r},${g},${b})`;
   getTrocaCorClass4.style.backgroundColor = corRGB;
-  // console.log(corRGB);
+
   // manter a cor ao recarregar
   const guardaCor1 = document.querySelector('.firstColor').style.backgroundColor;
-  // console.log(guardaCor1);
+
   const guardaCor2 = document.querySelector('.secondColor').style.backgroundColor;
-  // console.log(guardaCor2);
+
   const guardaCor3 = document.querySelector('.thirdColor').style.backgroundColor;
-  // console.log(guardaCor3);
+
   const guardaCor4 = document.querySelector('.fourtColor').style.backgroundColor;
-  // console.log(guardaCor4);
+
   let corAnterior = {
     guardaCor1,
     guardaCor2,
@@ -95,80 +113,90 @@ getButtonCorAleatoria.addEventListener('click', () => {
 });
 
 function boardInicio() {
-  let mainBoard = document.querySelector('#pixel-board');
-  for (let i = 0; i < 5; i += 1) {
-    for (let index = 0; index < 5; index += 1) {
+  const mainBoard = document.querySelector('#pixel-board');
+  const N = JSON.parse(localStorage.getItem('boardSize'));
+  for (let i = 0; i < N; i += 1) {
+    for (let index = 0; index < N; index += 1) {
       let pixelDoQuadro = document.createElement('div');
       pixelDoQuadro.className = 'pixel';
-      pixelDoQuadro.style.backgroundColor = 'rgb(255,255,255)';
-      pixelDoQuadro.style.height = '40px';
-      pixelDoQuadro.style.width = '40px';
-      pixelDoQuadro.style.border = '1px solid black';
-      pixelDoQuadro.style.display = 'inline-block';
+      // pixelDoQuadro.style.backgroundColor = 'rgb(255,255,255)';
+      // pixelDoQuadro.style.height = '40px';
+      // pixelDoQuadro.style.width = '40px';
+      // pixelDoQuadro.style.border = '1px solid black';
+      // pixelDoQuadro.style.display = 'inline-block';
       mainBoard.appendChild(pixelDoQuadro);
     }
     mainBoard.appendChild(document.createElement('br'));
   }
 }
 boardInicio();
-// Mudar classe selected
-const selecionaFirstColor = document.querySelectorAll('.color')[0];
-const selecionaSecondColor = document.querySelectorAll('.color')[1];
-const selecionaThirdColor = document.querySelectorAll('.color')[2];
-const selecionaFourtColor = document.querySelectorAll('.color')[3];
-// console.log(selecionaFirstColor);
-// console.log(selecionaSecondColor);
-// console.log(selecionaThirdColor);
-// console.log(selecionaFourtColor);
-// Mudar classe selected
-const corSelected = document.querySelectorAll('.color');
-selecionaFirstColor.addEventListener('click', () => {
-  for (let index = 0; index < corSelected.length; index += 1) {
-    corSelected[index].classList.remove('selected');
-  }
-  selecionaFirstColor.classList.add('selected');
-});
-selecionaSecondColor.addEventListener('click', () => {
-  for (let index = 0; index < corSelected.length; index += 1) {
-    corSelected[index].classList.remove('selected');
-  }
-  selecionaSecondColor.classList.add('selected');
-});
-selecionaThirdColor.addEventListener('click', () => {
-  for (let index = 0; index < corSelected.length; index += 1) {
-    corSelected[index].classList.remove('selected');
-  }
-  selecionaThirdColor.classList.add('selected');
-});
-selecionaFourtColor.addEventListener('click', () => {
-  for (let index = 0; index < corSelected.length; index += 1) {
-    corSelected[index].classList.remove('selected');
-  }
-  selecionaFourtColor.classList.add('selected');
-});
-// pintar quadro
 
-let getPixel = document.querySelectorAll('.pixel');
-for (let index = 0; index < getPixel.length; index += 1) {
-  const selectPixel = document.getElementsByClassName('pixel');
-  selectPixel[index].addEventListener('mousedown', (event) => {
-    event.target;
-    let pincel = document.querySelector('.selected');
-    let corDoPincel = pincel.style.backgroundColor;
-    // console.log(getPixel);
-    console.log(corDoPincel);
-    selectPixel[index].style.backgroundColor = corDoPincel;
+function geral() {
+  // Mudar classe selected
+  const selecionaFirstColor = document.querySelectorAll('.color')[0];
+  const selecionaSecondColor = document.querySelectorAll('.color')[1];
+  const selecionaThirdColor = document.querySelectorAll('.color')[2];
+  const selecionaFourtColor = document.querySelectorAll('.color')[3];
+
+  // Mudar classe selected
+  const corSelected = document.querySelectorAll('.color');
+  selecionaFirstColor.addEventListener('click', () => {
+    for (let index = 0; index < corSelected.length; index += 1) {
+      corSelected[index].classList.remove('selected');
+    }
+    selecionaFirstColor.classList.add('selected');
   });
-}
+  selecionaSecondColor.addEventListener('click', () => {
+    for (let index = 0; index < corSelected.length; index += 1) {
+      corSelected[index].classList.remove('selected');
+    }
+    selecionaSecondColor.classList.add('selected');
+  });
+  selecionaThirdColor.addEventListener('click', () => {
+    for (let index = 0; index < corSelected.length; index += 1) {
+      corSelected[index].classList.remove('selected');
+    }
+    selecionaThirdColor.classList.add('selected');
+  });
+  selecionaFourtColor.addEventListener('click', () => {
+    for (let index = 0; index < corSelected.length; index += 1) {
+      corSelected[index].classList.remove('selected');
+    }
+    selecionaFourtColor.classList.add('selected');
+  });
+  // pintar quadro
 
-// Botão apagar
-const getButtonLimpar = document.getElementById('clear-board');
-const getPixelClass = document.querySelectorAll('.pixel');
-const corApagar = 'rgb(255,255,255)';
-getButtonLimpar.addEventListener('click', () => {
-  for (let index = 0; index < getPixelClass.length; index += 1) {
-    // if (getPixelClass[index].style.background !== 'rgb(255,255,255)'){
-    getPixelClass[index].style.background = corApagar;
-    // }
+  let getPixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < getPixel.length; index += 1) {
+
+    const selectPixel = document.getElementsByClassName('pixel');
+    selectPixel[index].addEventListener('mousedown', (event) => {
+      // event.target;
+      const pincel = document.querySelector('.selected');
+      const corDoPincel = pincel.style.backgroundColor;
+      // console.log(getPixel);
+      // console.log(corDoPincel);
+      selectPixel[index].style.backgroundColor = corDoPincel;
+
+      // function guardaCor() {
+      // let matrizDeCor = document.getElementsByClassName('pixel');
+      // console.log(matrizDeCor);
+      // for (index = 0; index < matrizDeCor.length; index += 1) {
+
+      // guardaMatrizDeCor[index] = matrizDeCor[index].style.background;
+      // console.log(guardaMatrizDeCor);  
+      // }
+    });
+    // Botão apagar
+    const getButtonLimpar = document.getElementById('clear-board');
+    const getPixelClass = document.querySelectorAll('.pixel');
+    const corApagar = 'rgb(255,255,255)';
+    getButtonLimpar.addEventListener('click', () => {
+      for (let index = 0; index < getPixelClass.length; index += 1) {
+        // if (getPixelClass[index].style.background !== 'rgb(255,255,255)'){
+        getPixelClass[index].style.background = corApagar;
+        // }
+      }
+    });
   }
-});
+}
